61aba8dba5f732982b73bdd3a8edb7ee74192880
Oliver Beckstein <orbeckst@gmail.com>
2021-01-26 11:37:28 -0700

    fixed test a

--- tests/test_a.py
+++ tests/test_a.py
@@ -3,6 +3,6 @@ import pytest
 from .tst import _test_file as test_file_exists
 from .tst import _test_variable
 
-def test_a(name='a', reference=-1):
+def test_a(name='a', reference=1):
     return _test_variable(name, reference)
 
